2e7a254b837fa4d2901860f11e8e03b0f35263af
SlamperBOOM <slavashink@gmail.com>
2022-05-24 19:29:34 +0700

    Delete server directory

--- server/src/server/ServerData.java
+++
@@ -1,9 +0,0 @@
-package server.server;
-
-import java.util.List;
-
-public interface ServerData {
-    int getLowBorder();
-    List<String> getChat();
-    List<ClientInfo> getClients();
-}
--- server/src/server/commands/ServerCommand.java
+++
@@ -1,9 +0,0 @@
-package server.server.commands;
-
-import commands.Message;
-import server.server.Server;
-import server.server.ServerData;
-
-public interface ServerCommand {
-    void doCommand(Server server, ServerData data, Message message);
-}
