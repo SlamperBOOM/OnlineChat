c04123cb363716772ecdc15321111b128ca4b59a
SlamperBOOM <slavashink@gmail.com>
2022-05-24 19:29:42 +0700

    Delete client directory

--- client/src/UI/UI.java
+++
@@ -1,9 +0,0 @@
-package client.UI;
-
-import java.util.List;
-
-public interface UI {
-    void updateChat(List<String> chat);
-    void showUsers(List<String> users);
-    void showMessage(String text);
-}
--- client/src/client/commands/ClientCommand.java
+++
@@ -1,9 +0,0 @@
-package client.client.commands;
-
-import client.client.Client;
-import client.client.ClientData;
-import commands.Message;
-
-public interface ClientCommand {
-    void doCommand(Client client, ClientData data, Message message);
-}
